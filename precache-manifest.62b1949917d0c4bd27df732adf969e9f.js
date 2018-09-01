a36c44e2d027519378ddd0ff3bb138ca701a815b
liaohainan <liaohainan@liaohainandeMacBook-Pro.local>
2018-09-01 18:23:12 +0800

    dist

---
+++ precache-manifest.62b1949917d0c4bd27df732adf969e9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "8b56f310c684f787f937",
+    "url": "js/chunk-vendors.b790465e.js"
+  },
+  {
+    "revision": "cd42b60b5c30050cfb63",
+    "url": "js/app.f4459480.js"
+  },
+  {
+    "revision": "157569fac27e4d359960e2af9c70aa9f",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "8b56f310c684f787f937",
+    "url": "chunk-vendors.da6b1df0.css"
+  },
+  {
+    "revision": "cd42b60b5c30050cfb63",
+    "url": "app.ab357fe2.css"
+  }
+];
